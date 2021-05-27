72d56e38ffa6e9ed6fd042917305167cf7b01651
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-27 10:10:52 -0500

    Create arreglosconif.py

---
+++ arreglosconif.py
@@ -0,0 +1,16 @@
+"""ARREGLOS UNIDIMENSIONALES"""
+"""GUARDAR en un arreglo los numeros pares y en otro arreglo los numeros impares
+comprendidos del 1 al 100 e imprimirlos por la pantalla"""
+
+
+par = []
+impar = []
+
+for i in range(1,101):
+    if i%2==0:
+        par.append(i)
+    else:
+        impar.append(i)
+
+print (par)
+print(impar)
