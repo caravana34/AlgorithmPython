49c5c7c04531c07c4beb372a40127dbfcf553a68
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-27 10:01:43 -0500

    Create guia.py

---
+++ guia.py
@@ -0,0 +1,34 @@
+#variable de tipo entero
+edad= 35
+# variable de tipo cadena
+nom= "Darío"
+#variable de tipo decimal
+peso = 84.4
+#variable de tipo boolean
+valor= True
+
+
+print(edad)
+print(nom)
+print(peso)
+print(valor)
+
+# concatenar
+print( "edad =" + str (edad))
+print ("nombre =" + nom)
+print (peso)
+print(valor)
+
+
+#opereaciones + - / *//**%
+num1=10
+num2=2
+print(num1 + num2)
+print(num1/num2)
+print(num1//num2)
+
+num1= 254
+num2= 12456
+ra = num1 + num2 
+print (num1 + num2)
+print ("resultado=" + str(ra))
