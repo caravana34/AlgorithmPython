ecd08eabe503e6549d61dec2239eae42297269cd
J Darío Gómez <44247083+caravana34@users.noreply.github.com>
2021-05-27 10:18:25 -0500

    Create igualestresnumeros.py

---
+++ igualestresnumeros.py
@@ -0,0 +1,8 @@
+n2 = eval(input("ingrese numero 3"))
+n3 = eval(input("ingrese numero 4"))
+if n2 > n3:
+    print("numero 3 es mayor")
+elif n2<n3:
+    print("numero 4 es mayor")
+else:
+    print ("son iguales")
